148b4c0a98a18672423dfaee1d4b7799dbfc8355
ss <ss>
2019-05-13 22:31:43 +0900

    add double struct

--- hiho-ad-test/hiho-ad-test/Main.cpp
+++ hiho-ad-test/hiho-ad-test/Main.cpp
@@ -4,4 +4,5 @@ using namespace hiho;
 
 int main() {
 	ad00_primitive_double(100, 0.2, 100, 0.005, 3, 1000);
+	ad01_double_struct   (100, 0.2, 100, 0.005, 3, 1000);
 }
--- hiho-ad-test/hiho-ad-test/hiho/ad.hpp
+++ hiho-ad-test/hiho-ad-test/hiho/ad.hpp
@@ -1,3 +1,4 @@
 #pragma once
 
 #include "ad00_primitive_double.hpp"
+#include "ad01_double_struct.hpp"
