2f6ecd65e30318b1e7a47a0c4f882e7a71c90150
SS <zakktanaka@hotmail.com>
2019-06-19 23:09:11 +0900

    logger

--- hiho-ad/test/hiho-ad-test/Main.cpp
+++ hiho-ad/test/hiho-ad-test/Main.cpp
@@ -1,6 +1,10 @@
 #include <Test.hpp>
 
 int main(int argc, char** argv) {
+
+	auto logger = spdlog::stdout_color_mt("LOG");
+	spdlog::set_default_logger(logger);
+
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
 }
--- hiho-ad/test/hiho-ad-test/test.cpp
+++ hiho-ad/test/hiho-ad-test/test.cpp
@@ -1,6 +1,6 @@
 #include <Test.hpp>
 
 TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+	EXPECT_EQ(1, 1);
+	EXPECT_TRUE(true);
 }
